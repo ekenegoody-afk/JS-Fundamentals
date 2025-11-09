efc786f45437122100e0820c39185b5a25ee51de
Nyerhovwo otorie <ekenegoody@gmail.com>
2025-11-09 21:10:41 +0100

    typo corrected

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-const list =['C is fun', 'Pyhton is cool', 'Javascript is amazing'];
+const list =['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i=0;i<list.length;i++){
     console.log(list[i]);
 }
