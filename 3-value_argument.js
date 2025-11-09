07d55efb95529c6eec713d51e85dea24adb844a1
Nyerhovwo otorie <ekenegoody@gmail.com>
2025-11-09 20:20:19 +0100

    Added 3-value_argument.js

---
+++ 3-value_argument.js
@@ -0,0 +1,16 @@
+const arg= process.argv[2];
+if (args == undefined){
+    console.log("No argument");
+} else{
+    console.log(arg);
+}
+
+
+
+// const arg = process.argv[2];
+
+// if (arg === undefined) {
+//   console.log("No argument");
+// } else {
+//   console.log(arg);
+// }
