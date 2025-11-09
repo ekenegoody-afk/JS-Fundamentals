0700402be8e857d61a84dd6a1ae98e865e4ac687
Nyerhovwo otorie <ekenegoody@gmail.com>
2025-11-09 21:40:09 +0100

    a file added

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,7 +1,12 @@
 let arg = process.argv[2];
 let num = Number(arg);
 
-while (num!=0) {
+if (!isNaN(num)){
+    while (num!=0) {
     console.log("C is fun");
     num--;
+}
+
+}else{
+    console.log("Missing number of occurences");
 }
